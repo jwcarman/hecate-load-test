e59cfaefc8781d12fb06b70651209632fca54922
James Carman <jcarman@savoirtech.com>
2016-03-27 06:44:23 -0400

    Re-enabling

--- gatling/src/test/scala/com/savoirtech/hecate/load/gatling/RecordedSimulation.scala
+++ gatling/src/test/scala/com/savoirtech/hecate/load/gatling/RecordedSimulation.scala
@@ -9,19 +9,20 @@ import scala.language.postfixOps
 class RecordedSimulation extends Simulation {
 
   val httpProtocol = http
-    .baseURL("http://localhost:8888")
+    .baseURL("http://localhost:9999")
     .inferHtmlResources()
     .acceptHeader("text/plain")
     .acceptEncodingHeader("gzip, deflate, sdch")
 
   val repeat = System.getProperty("repeat", "1000")
   val users = System.getProperty("users", "500")
-  val duration = System.getProperty("duration", "10")
+  val duration = System.getProperty("duration", "60")
 
-  val scn = scenario("foo").repeat(repeat.toInt) {
+  val scn = scenario("hello").repeat(repeat.toInt) {
     exec(http("request_0")
-      .get("/").check(status.is(200)).check(bodyString.is("Hello, World!")))
+      .get("/services/hello/jcarman").check(status.is(200)).check(bodyString.is("Hello, Jim!")))
   }
+
 
 
   setUp(scn.inject(rampUsers(users.toInt) over (duration.toInt seconds)))
