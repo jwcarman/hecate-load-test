9ad5c9ab35c093143aa51b0a772573ab4e0143d2
James Carman <jcarman@savoirtech.com>
2016-03-28 16:10:52 -0400

    Fixing compile error.

--- web/src/main/java/com/savoirtech/hecate/load/web/config/HecateConfig.java
+++ web/src/main/java/com/savoirtech/hecate/load/web/config/HecateConfig.java
@@ -1,13 +1,18 @@
 package com.savoirtech.hecate.load.web.config;
 
-import com.codahale.metrics.JmxReporter;
 import com.datastax.driver.core.Cluster;
 import com.datastax.driver.core.Session;
 import com.datastax.driver.core.schemabuilder.Create;
-import com.savoirtech.hecate.core.metrics.HecateMetrics;
 import com.savoirtech.hecate.load.web.domain.Person;
+import com.savoirtech.hecate.pojo.binding.PojoBindingFactory;
+import com.savoirtech.hecate.pojo.binding.def.DefaultPojoBindingFactory;
+import com.savoirtech.hecate.pojo.convert.def.DefaultConverterRegistry;
 import com.savoirtech.hecate.pojo.dao.PojoDaoFactory;
 import com.savoirtech.hecate.pojo.dao.def.DefaultPojoDaoFactory;
+import com.savoirtech.hecate.pojo.facet.field.FieldFacetProvider;
+import com.savoirtech.hecate.pojo.naming.def.DefaultNamingStrategy;
+import com.savoirtech.hecate.pojo.query.def.DefaultPojoQueryContextFactory;
+import com.savoirtech.hecate.pojo.statement.def.DefaultPojoStatementFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
@@ -15,9 +20,16 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class HecateConfig {
+//----------------------------------------------------------------------------------------------------------------------
+// Fields
+//----------------------------------------------------------------------------------------------------------------------
 
     private static final String KEYSPACE_NAME = "hecate_load";
     private static final Logger LOGGER = LoggerFactory.getLogger(HecateConfig.class);
+
+//----------------------------------------------------------------------------------------------------------------------
+// Other Methods
+//----------------------------------------------------------------------------------------------------------------------
 
     @Bean
     public PojoDaoFactory createPojoDaoFactory() {
@@ -26,10 +38,11 @@ public class HecateConfig {
         Session session = cluster.newSession();
         session.execute(String.format("CREATE KEYSPACE IF NOT EXISTS %s WITH replication = {'class':'SimpleStrategy', 'replication_factor':1};", KEYSPACE_NAME));
         session.close();
-        JmxReporter.forRegistry(HecateMetrics.REGISTRY).inDomain("hecate").build().start();
         session = cluster.connect(KEYSPACE_NAME);
-        DefaultPojoDaoFactory daoFactory = new DefaultPojoDaoFactory(session);
-        Create create = daoFactory.getBindingFactory().createPojoBinding(Person.class).createTable("person");
+        DefaultNamingStrategy namingStrategy = new DefaultNamingStrategy();
+        PojoBindingFactory bindingFactory = new DefaultPojoBindingFactory(new FieldFacetProvider(), new DefaultConverterRegistry(), namingStrategy);
+        DefaultPojoDaoFactory daoFactory = new DefaultPojoDaoFactory(session, bindingFactory, new DefaultPojoStatementFactory(session), new DefaultPojoQueryContextFactory(session, new DefaultPojoStatementFactory(session)), namingStrategy);
+        Create create = bindingFactory.createPojoBinding(Person.class).createTable("person");
         session.execute(create);
         return daoFactory;
     }
